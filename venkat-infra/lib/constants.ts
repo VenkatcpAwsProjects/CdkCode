d7c71ec03fd1410dc2c3a0018f510e57f78dc7f4
Venkatesan Panneerselvam <venkatcp@amazon.com>
2022-05-26 19:42:00 +0530

    ading constants

---
+++ venkat-infra/lib/constants.ts
@@ -0,0 +1,18 @@
+export class Constants
+{
+    public  static readonly AmzSQSQueue : string = 'AmzSQSQueue';
+    public  static readonly producerLambda : string = 'producer-lambda';
+    public  static readonly consumerLambda : string = 'consumer-lambda';
+    public  static readonly statusCheckLambda : string = 'status-check-lambda';
+    public  static readonly AmzAppealStatusTable : string = 'AmzAppealStatusTable';
+    public  static readonly appealId : string = 'appealId';
+    public  static readonly appealDedupId : string = 'appealDedupId';
+    public  static readonly AmzAppealTable : string = 'AmzAppealTable';
+    public  static readonly AmzProducerLambda : string = 'AmzProducerLambda';
+    public  static readonly AmzConsumerLambda : string = 'AmzConsumerLambda';
+    public  static readonly AmzGetAppealStatusLambda : string = 'AmzGetAppealStatusLambda';
+    public  static readonly appeal : string = 'appeal';
+    public  static readonly appealStatus : string = 'appealstatus';
+    public  static readonly GET : string = 'GET';
+    
+}
--- venkat-infra/lib/venkat-infra-stack.ts
+++ venkat-infra/lib/venkat-infra-stack.ts
@@ -1,3 +1,4 @@
+
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
@@ -9,54 +10,36 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { AccountPrincipal, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Constants } from './constants';
 
 export class VenkatInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    //The code to create role and policy
-    var accId = "161069081910";
-    var a = new AccountPrincipal(accId);
-    const role = new iam.Role(this, 'CategoryDetailsMapperExternalAwsRole', {
-      roleName: 'CategoryDetailsMapperExternalAwsRole',
-      assumedBy: a,
-      description: 'External AWS role for other AWS account['+ accId +'] to read the Category Details Mapper AWS App config.',
-    });
-
-    // 👇 Create a Managed Policy and associate it with the role
-    const managedPolicy = new iam.ManagedPolicy(this, 'CategoryDetailsMapperAppConfigReadPolicy', {
-      managedPolicyName: "CategoryDetailsMapperAppConfigReadPolicy",
-      description: 'Allows app-config read access',
-      statements: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ['appconfig:GetLatestConfiguration', 'appconfig:StartConfigurationSession'],
-          resources: ['arn:aws:appconfig:us-east-1:638373575924:application/CategoryDetailsConfig/environment/CategoryDetailsConfig-env/configuration/CategoryDetailsConfigProfile-S3BasedConfigurationProfile'],
-        }),
-      ],
-      roles: [role],
-    });
-
     // The code that defines your stack goes here
 
-    var amzSQSQueue = new sqs.Queue(this, 'AmzSQSQueue', 
+    var amzSQSQueue = new sqs.Queue(this, Constants.AmzSQSQueue, 
     {
-       queueName: 'AmzSQSQueue',
+       queueName: Constants.AmzSQSQueue,
        "visibilityTimeout" : Duration.minutes(3)
     });
 
-    var producerPath = path.join(__dirname, 'producer-lambda');
-    var consumerPath = path.join(__dirname, 'consumer-lambda');
-    var statusCheckPath = path.join(__dirname, 'status-check-lambda');
+    var producerPath = path.join(__dirname, Constants.producerLambda);
+    var consumerPath = path.join(__dirname, Constants.consumerLambda);
+    var statusCheckPath = path.join(__dirname, Constants.statusCheckLambda);
 
-    const amzTable = new dynamodb.Table(this, 'AmzAppealTable', {
-      tableName: 'AmzAppealTable',
-      partitionKey: { name: 'appealId', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'appealDedupId', type: dynamodb.AttributeType.STRING }
+    const amzAppealStatusTable = new dynamodb.Table(this, Constants.AmzAppealStatusTable, {
+      tableName: Constants.AmzAppealStatusTable,
+      partitionKey: { name: Constants.appealId, type: dynamodb.AttributeType.STRING },
+      sortKey: { name: Constants.appealDedupId, type: dynamodb.AttributeType.STRING }
     });
 
-    var amzProducerLambda = new lambda.Function(this, 'AmzProducerLambda', {
-      functionName: 'AmzProducerLambda',
+    const amzAppealTable = new dynamodb.Table(this, Constants.AmzAppealTable, {
+      tableName: Constants.AmzAppealTable,
+      partitionKey: { name: Constants.appealId, type: dynamodb.AttributeType.STRING }
+    });
+
+    var amzProducerLambda = new lambda.Function(this, Constants.AmzProducerLambda, {
+      functionName: Constants.AmzProducerLambda,
       runtime: lambda.Runtime.PYTHON_3_9,
       handler: 'lambda2.lambda_handler',
       code: lambda.Code.fromAsset(producerPath),
@@ -66,39 +49,41 @@ export class VenkatInfraStack extends Stack {
       },
     });
 
-    var amzConsumerLambda = new lambda.Function(this, 'AmzConsumerLambda', {
-      functionName: 'AmzConsumerLambda',
+    var amzConsumerLambda = new lambda.Function(this, Constants.AmzConsumerLambda, {
+      functionName: Constants.AmzConsumerLambda,
       runtime: lambda.Runtime.PYTHON_3_9,
       handler: 'lambda1.lambda_handler',
       code: lambda.Code.fromAsset(consumerPath),
       timeout : Duration.minutes(2),
       environment : {
-        "amz_db_name": amzTable.tableName,
+        "amz_db_name": amzAppealTable.tableName,
       },
     });
 
-    var amzGetAppealStatusLambda = new lambda.Function(this, 'AmzGetAppealStatusLambda', {
-      functionName: 'AmzGetAppealStatusLambda',
+    var amzGetAppealStatusLambda = new lambda.Function(this, Constants.AmzGetAppealStatusLambda, {
+      functionName: Constants.AmzGetAppealStatusLambda,
       runtime: lambda.Runtime.PYTHON_3_9,
       handler: 'lambda3.lambda_handler',
       code: lambda.Code.fromAsset(statusCheckPath),
       timeout : Duration.minutes(2),
       environment : {
-        "amz_db_name": amzTable.tableName,
+        "amz_db_name": amzAppealTable.tableName,
       },
     });
 
-    const apiGateWay = new apigateway.RestApi(this, 'appeal');
-    const appealStatus = apiGateWay.root.addResource('appealstatus');
-    appealStatus.addMethod('GET', new apigateway.LambdaIntegration(amzGetAppealStatusLambda, 
+    const apiGateWay = new apigateway.RestApi(this, Constants.appeal);
+    const appealStatus = apiGateWay.root.addResource(Constants.appealStatus);
+    appealStatus.addMethod(Constants.GET, new apigateway.LambdaIntegration(amzGetAppealStatusLambda, 
       {
         proxy: true,
       }));
 
 
     amzSQSQueue.grantSendMessages(amzProducerLambda);
-    amzTable.grantReadWriteData(amzConsumerLambda);
-    amzTable.grantReadWriteData(amzGetAppealStatusLambda);
+    amzAppealStatusTable.grantReadWriteData(amzConsumerLambda);
+    amzAppealStatusTable.grantReadWriteData(amzGetAppealStatusLambda);
+    amzAppealTable.grantReadWriteData(amzProducerLambda);
+    amzAppealTable.grantReadWriteData(amzGetAppealStatusLambda);
 
     amzConsumerLambda.addEventSource(
       new SqsEventSource(amzSQSQueue, {
